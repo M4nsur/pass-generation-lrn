45efd0ad37d7c232d45a9abd65d0b09eec257e64
Mansur <vakhabov.mans@gmail.com>
2025-11-18 02:56:55 +0300

    add methods for account struct

--- main.go
+++ main.go
@@ -6,17 +6,42 @@ import (
 )
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-func main () {
-	pass := generatePassword(25)
-	fmt.Println(pass, len(pass))
+type account struct {
+	login string
+	password string
+	url string
 }
 
-func generatePassword(n int) string {
-	
+func (acc account) printAccount() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int)  {
 	var pass = make([]rune, n)
 	for i := range n {
 		pass[i] = letterRunes[rand.IntN(len(letterRunes))]
 
 	}
-	return string(pass)
+	acc.password = string(pass)
+}
+
+func main () {
+	login := promptData("Введите логин")
+	url := promptData("Введите url")
+
+	myAcc := account{
+		login: login,
+		url: url,
+	}
+
+	myAcc.generatePassword(15)
+	myAcc.printAccount()
+
+}
+
+func promptData (message string) string {
+	var res string
+	fmt.Println(message)
+	fmt.Scan(&res)
+	return res
 }
